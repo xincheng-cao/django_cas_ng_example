3d8664b67e113b983ed7aebaa99c6e49e848d6b9
shiba <36287551+xincheng-cao@users.noreply.github.com>
2023-03-31 17:29:11 +0800

    update mysite/views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 
 def index(request):
     if request.user.is_authenticated:
-        return HttpResponse('<p>Welcome to <a href="https://djangocas.dev">django-cas-ng</a>.</p><p>You logged in as <strong>%s</strong>.</p><p><a href="/accounts/logout">Logout</a></p>' % request.user)
+        return HttpResponse(
+            '''
+            <p>Welcome to <a href="https://djangocas.dev">django-cas-ng</a>.</p><p>You logged in as <strong>{}</strong>.</p><p><a href="/accounts/logout">Logout</a></p>
+            <iframe height="500" width="800" src="http://10.200.0.43:8502" title="W3Schools Free Online Web Tutorials"></iframe>
+            '''.format(request.user)
+        )
     else:
         return HttpResponse('<p>Welcome to <a href="https://djangocas.dev">django-cas-ng</a>.</p><p><a href="/accounts/login">Login</a></p>')
